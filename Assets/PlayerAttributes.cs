28fea8389870ff9a1021e1893511236463d41351
chin6902 <chin6902@gmail.com>
2023-12-01 10:58:16 +0900

    12/01

--- Assets/PlayerAttributes.cs
+++ Assets/PlayerAttributes.cs
@@ -4,7 +4,7 @@ public class PlayerAttributes : MonoBehaviour
 {
     // Static variables accessible from other scripts
     public static int points = 0;
-    public static int health = 100;
+    public static int health = 50;
     public static float speed = 2.0f;
     public static int toughness = 1;
 }
--- Assets/enemySlime.cs
+++ Assets/enemySlime.cs
@@ -7,13 +7,13 @@ public class enemySlime : MonoBehaviour
     public float moveSpeed = 2f; // Speed at which the slime moves
     private bool moveRight = true; // Determines the initial direction
 
-    private Animator slimeAnimator; // Reference to the Animator component
+    private Animator animator; // Reference to the Animator component
 
     // Start is called before the first frame update
     void Start()
     {
-        // Get the Animator component attached to the GameObject
-        slimeAnimator = GetComponent<Animator>();
+        // Get the Animator component attached to the same GameObject
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -23,10 +23,12 @@ public class enemySlime : MonoBehaviour
         if (moveRight)
         {
             transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
+            animator.SetFloat("Horizontal", 1);
         }
         else
         {
             transform.Translate(Vector2.left * moveSpeed * Time.deltaTime);
+            animator.SetFloat("Horizontal", 0);
         }
 
         // Check if the slime should change direction
